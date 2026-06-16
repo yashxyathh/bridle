ba06b44a0c21226c645bdcf264265848674b6349
YASHASVI YATHARTH <yashasviyatharth07@gmail.com>
2026-06-16 17:21:18 +0530

    Add GuessInput widget with TextField for user input handling

--- lib/main.dart
+++ lib/main.dart
@@ -81,26 +81,36 @@ class Tile extends StatelessWidget {
 class GuessInput extends StatelessWidget {
   GuessInput({super.key, required this.onSubmitGuess});
   final void Function(String) onSubmitGuess;
+  final TextEditingController _textEditingController = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
 
   @override
   Widget build(BuildContext) {
-
     return Row(
-      children:[
+      children: [
         Expanded(
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextField(
               maxLength: 5,
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.all(Radius.circular(35)),
-                )
+                ),
               ),
-            )
-          )
-        )
-      ]
+              controller: _textEditingController,
+              autofocus: true,
+              focusNode: _focusNode,
+
+              onSubmitted: (input) {
+                print(input);
+                _textEditingController.clear();
+                _focusNode.requestFocus();
+              },
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
